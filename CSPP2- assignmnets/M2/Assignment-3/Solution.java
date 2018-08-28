dd8866b23a4d08ac3bf5e12dd9bfe81ee9306e59
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-28 19:12:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M2/Assignment-3/Solution.java
+++ CSPP2- assignmnets/M2/Assignment-3/Solution.java
@@ -27,7 +27,8 @@ public final class Solution {
          * {Recursive function to find the power }
          */
         if (exponent != 0) {
-            return (base * power(base, exponent - 1));}
+            return (base * power(base, exponent - 1)); 
+        }
         else {
             return 1;
         }
