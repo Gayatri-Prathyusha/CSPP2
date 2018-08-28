293fde7d719aa9e619797cb92bd0d5f79f5e3a6b
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-28 18:02:34 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2- assignmnets/M2/Assignment-2/Solution.java
+++ CSPP2- assignmnets/M2/Assignment-2/Solution.java
@@ -14,18 +14,30 @@ public final class Solution {
          * { item_description }
          */
     }
+    /**.
+     * { function_description }
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
     public static void rootsOfQuadraticEquation(int a,int b,int c){
-        /**
+        /**.
          * { Function definition }
          */
         double sq = java.lang.Math.sqrt((b * b) - (2 * 2 * a * c));
-        double quad1 = ((-b + sq)/(2 * a));
-        double quad2 = ((-b - sq)/(2 * a));
-        System.out.println(quad1 +" "+ quad2);
+        double quad1 = ((-b + sq) / (2 * a));
+        double quad2 = ((-b - sq) / (2 * a));
+        System.out.println(quad1 + " " + quad2);
 
     }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
-        /**
+        /**.
          * { main function }
          */
         Scanner scan = new Scanner(System.in);
@@ -34,7 +46,7 @@ public final class Solution {
         int c = scan.nextInt();
         rootsOfQuadraticEquation(a, b, c);
     }
-    /*
+    /**.
     Need to write the rootsOfQuadraticEquation function and print the output.
     */
 }
