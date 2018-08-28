5ddd5b90b6d26c6a7e6d5025451f672d1bbcd1ad
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-28 17:16:09 +0530

    Factorial using recursion

---
+++ CSPP2- practice/M2/Factorial recursion/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.Math.*;
+class Factorial{
+	public static int fact(int n){
+        if (n==1)
+        	return 1;
+        else
+		    return n*fact(n-1);
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Factorial of the given number is : " + fact(n));
+
+	}
+}
