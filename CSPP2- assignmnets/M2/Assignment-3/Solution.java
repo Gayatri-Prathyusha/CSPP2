d1cadb60c8bc9b643ad1262d2b7e03a22fa941d1
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-28 19:14:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- CSPP2- assignmnets/M2/Assignment-3/Solution.java
+++ CSPP2- assignmnets/M2/Assignment-3/Solution.java
@@ -28,8 +28,7 @@ public final class Solution {
          */
         if (exponent != 0) {
             return (base * power(base, exponent - 1));
-        }
-        else {
+        } else {
             return 1;
         }
     }
