570514c8a1f43d2ff8e27578a39e20fb8d59ad1c
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-29 12:14:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M3/Assignment-2 2/Solution.java
+++ CSPP2- assignmnets/M3/Assignment-2 2/Solution.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 	*/
 
 public class Solution {
-	static int number0f7s(int n) {
+	static int numberof7s(int n) {
     int count = 0;
     while (n > 0) {
         if (n % 10 == 7)
@@ -18,7 +18,7 @@ public class Solution {
  
     // Counts the number of '7' 
     // digits between 0 and n 
-    static int numberOf7sinRange(int n) {
+    static int numberinrange(int n) {
      
         // Initialize result
         int count = 0; 
@@ -26,7 +26,7 @@ public class Solution {
         // Count 7's in every number 
         // from 7 to n
         for (int i = 7; i <= n; i++)
-        count += number0f7s(i);
+        count += numberof7s(i);
  
         return count;
 }
@@ -34,7 +34,7 @@ public class Solution {
         
         Scanner s=new Scanner(System.in);      
         int n = s.nextInt();
-        System.out.println(numberOf7sinRange(n));
+        System.out.println(numberinrange(n));
 
 
         
