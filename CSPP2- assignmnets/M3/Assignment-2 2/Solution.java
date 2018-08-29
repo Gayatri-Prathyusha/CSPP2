af57446b533d0435a2c5e481648eafbe55935c9f
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-29 12:20:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M3/Assignment-2 2/Solution.java
+++ CSPP2- assignmnets/M3/Assignment-2 2/Solution.java
@@ -1,24 +1,61 @@
-
+/**.
+ *{ To get the number of 7's present until n value }
+ */
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
+/**.
+ * Class for solution.
+ */
 
 public class Solution {
+	/**.
+	 * Constructs the object.
+	 */
+	private Solution() {
+		/**.
+		 * { item_description }
+		 */
+	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      n     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	static int numberof7s(int n) {
+		/**.
+		 * { Function to return the number of 7's present in one number}
+		 */
     int count = 0;
     while (n > 0) {
+    	/**.
+    	 * { item_description }
+    	 */
         if (n % 10 == 7)
         count++;
  
         n = n / 10;
     }
+    /**.
+     * { Returns the value if the 7 is present or not and how many are present}
+     */
     return count;
 }
  
     // Counts the number of '7' 
-    // digits between 0 and n 
+    // digits between 0 and n
+
+    /**.
+     * { function_description }
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     static int numberinrange(int n) {
+    	/**.
+    	 * { Function to retuen the Number of 7's present until n value }
+    	 */
      
         // Initialize result
         int count = 0; 
@@ -27,18 +64,21 @@ public class Solution {
         // from 7 to n
         for (int i = 7; i <= n; i++)
         count += numberof7s(i);
- 
         return count;
 }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
+    	/**.
+    	 * { Main function}
+    	 */
         
         Scanner s=new Scanner(System.in);      
         int n = s.nextInt();
         System.out.println(numberinrange(n));
-
-
-        
-       
-        
+ 
     }
 }
