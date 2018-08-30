625ab60bdf4be9facb5a81d0da53a32bf19d4495
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-30 12:10:13 +0530

    find 6

--- CSPP2- practice/M4/First last 6 array/FirstLast6.java
+++ CSPP2- practice/M4/First last 6 array/FirstLast6.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class FirstLast6 {
-	public static void main (String[] args) {
+    public static boolean find(int[] arr, int n) {
+        
+        return arr[0] == 6 || arr[n-1] == 6;
+    }
+    public static void main (String[] args) {
 
     
-	Scanner s = new Scanner(System.in);
-	int n = s.nextInt();
-	int[] a = new int[3];
-	//int len = a.length-1;
-	for (int i = 0; i < n; i++) {
-		int b = s.nextInt();
-		a[i] = b;
-	}
-	if (a[0] == 6 || a[n-1] == 6)
-		System.out.println("true");
-	else 
-		System.out.println("false");
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int[] a = new int[3];
+    //int len = a.length-1;
+    for (int i = 0; i < n; i++) {
+        int b = s.nextInt();
+        a[i] = b;
+    }
+    System.out.println(find (a, n));
+    
 }
 }
