26dcfa5b376a1d8ef4ccdcbb764f5d57095f745c
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-30 14:09:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2- assignmnets/M4/Assignment-1/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-1/Solution.java
@@ -1,28 +1,61 @@
-
+/**
+ * { program to find the largest numbers in the given array}
+ */
 import java.util.*;
+
+/**
+ * Class for solution.
+ */
 public class Solution
 {/*
-	Fill this main function to print maximum of given array
-	*/
-	public static int largest(int[] arr, int n) {
+    Fill this main function to print maximum of given array
+    */
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { item_description }
+         */
+    }
+    /**
+     * { function_description }
+     *
+     * @param      arr   The arr
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int largest(final int[] arr, final int n) {
+        /**
+         * { var_description }
+         */
         int max = arr[0];
-		for(int i = 0; i < n; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		return max;
-	}
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			int b = sc.nextInt();
-			a[i] = b;
-		}
-	System.out.println(largest(a, n));
+        for(int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args)
+    {
+        /**
+         * { var_description }
+         */
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            int b = sc.nextInt();
+            a[i] = b;
+        }
+    System.out.println(largest(a, n));
 
-	}
+    }
 }
