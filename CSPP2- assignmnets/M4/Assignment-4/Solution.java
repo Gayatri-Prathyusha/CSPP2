bf4a6f894415bdaec7a13b284b1ae6574be688e4
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 14:00:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M4/Assignment-4/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-4/Solution.java
@@ -1,28 +1,57 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
+/**
+ * { Given an String, Write a java method that returns the decimal 
+ * value for the given binary string. }
+ */
 import java.util.Scanner;
 import java.lang.*;
-public class Solution
+/**
+ * Class for solution.
+ */
+public final class Solution
 {/*
-	Do not modify the main function 
-	*/
-	public static String reverseString(String str) {
-		String str1 = "";
-		for (int i = str.length()-1 ; i >= 0 ; i--) {
-			str1 = str1 + str.charAt(i) ;
-		}
-		return str1;
-	}
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
+    Do not modify the main function 
+    */
+    /**
+     * Constructs the object.
+     */
+    private Solution () {
+        /**
+         * { item_description }
+         */
+    }
+    /**
+     * { function_description }
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(final String str) {
+        /**
+         * { var_description }
+         */
+        String str1 = "";
+        for (int i = str.length()-1 ; i >= 0 ; i--) {
+            str1 = str1 + str.charAt(i) ;
+        }
+        return str1;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);  
+        System.out.println(reverse);
+        
+    }
+    //Write reverseString function
 
 }
