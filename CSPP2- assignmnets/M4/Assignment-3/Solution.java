0546eb2cb24a8d41167c30ab6e8f9427ba4989b2
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 12:36:04 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- CSPP2- assignmnets/M4/Assignment-3/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-3/Solution.java
@@ -3,13 +3,23 @@
 
 
 import java.util.Scanner;
+import java.lang.*;
 public class Solution
 {/*
 	Do not modify this main function.
 	*/
 	public static int binaryToDecimal(String num) {
-         return Integer.parseInt(num, 2);
-             
+         int dec = Integer.parseInt(num, 2);
+         //System.out.println();
+         // int dec = 0;
+         // int p = 0;
+         // while (num != null) {
+         //    dec+=((num % 10) * Math.pow(2,p));
+         //    num = num / 10;
+         //    p++;
+         //    }
+            
+            return dec;
 		}
 
 	
@@ -18,8 +28,8 @@ public class Solution
 		Scanner sc=new Scanner(System.in);
 		int n= sc.nextInt();
 		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
-			int res=binaryToDecimal(s);//Write binaryToDecimal function
+			String s = sc.nextLine();
+			int res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
