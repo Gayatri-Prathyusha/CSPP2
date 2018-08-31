bde0dc3ee9955b2ddc2bd65c8fd9b3628dcd76c7
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 11:58:09 +0530

    Concatination of strings new

--- CSPP2- practice/M4/Concatination/Concatinate.java
+++ CSPP2- practice/M4/Concatination/Concatinate.java
@@ -1,12 +1,15 @@
 import java.util.*;
 import java.lang.*;
 public class Concatinate {
+	public static String helloName(String s, String s1) {
+		return s.concat(s1)+"!";
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = new String("Hello ");
 		String str1 =sc.next();
-      
-		System.out.println(str.concat(str1)+"!");
+        
+		System.out.println(helloName(str, str1));
 
 	}
 }
