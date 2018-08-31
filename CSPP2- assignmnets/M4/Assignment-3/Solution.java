504f15af5f06158c55150346f439077274a0ba2d
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 13:34:59 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- CSPP2- assignmnets/M4/Assignment-3/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-3/Solution.java
@@ -1,24 +1,24 @@
-/**
+/**.
  * {Given an String, Write a java method that returns the
  * decimal value for the given binary string.}
  */
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
     /*
     Do not modify this main function.
     */
-    /**
+    /**.
      * Constructs the object.
      */
     private Solution() {
-        /**
+        /**.
          * { item_description }
          */
     }
-    /**
+    /**.
      * { function_description }
      *
      * @param      num   The number
@@ -26,29 +26,29 @@ public final class Solution {
      * @return     { description_of_the_return_value }
      */
     public static int binaryToDecimal(final String num) {
-        /**
+        /**.
          * { var_description }
          */
          int dec = Integer.parseInt(num, 2);
             return dec;
         }
 
-    /**
+    /**.
      * { function_description }
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        /**
+        /**.
          * { var_description }
          */
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0 ; i <= n ; i++) {
+        for (int i = 0; i <= n; i++) {
             String s = sc.nextLine();
             int res = 0;
             if (!s.isEmpty()) {
-                res = binaryToDecimal(s);//Write binaryToDecimal function
+                res = binaryToDecimal(s); //Write binaryToDecimal function
                 System.out.println(res);
             }
         }
