af14119b59199137b02b6966b1dfa9c38063db76
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 13:32:39 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- CSPP2- assignmnets/M4/Assignment-3/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-3/Solution.java
@@ -1,13 +1,13 @@
 /**
- * {Given an String, Write a java method that returns the 
+ * {Given an String, Write a java method that returns the
  * decimal value for the given binary string.}
  */
 import java.util.Scanner;
 /**
  * Class for solution.
  */
-public final class Solution
-{/*
+public final class Solution {
+    /*
     Do not modify this main function.
     */
     /**
