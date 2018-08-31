c2545ed031c673d19700a79feb11d87a87f2bcbe
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 13:49:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M4/Assignment-4/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-4/Solution.java
@@ -11,7 +11,7 @@ public class Solution
 	public static String reverseString(String str) {
 		String str1 = "";
 		for (int i = 0; i < str.length(); i++) {
-			str1 = str1 + str.charAt(i);
+			str1 = str.charAt(i) + str1;
 		}
 		return str1;
 	}
