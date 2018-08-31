237d0c03bca671b134c59d87b7351ccfed6f5ac9
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-31 14:07:02 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- CSPP2- assignmnets/M4/Assignment-4/Solution.java
+++ CSPP2- assignmnets/M4/Assignment-4/Solution.java
@@ -30,7 +30,7 @@ public final class Solution {
          * { var_description }
          */
         String str1 = "";
-        for (int i = str.length()-1; i >= 0; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             str1 = str1 + str.charAt(i);
         }
         return str1;
