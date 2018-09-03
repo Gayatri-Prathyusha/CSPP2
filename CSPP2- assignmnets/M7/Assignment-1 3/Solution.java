436538cd00c9ca884124f9ec069232ee73fabd76
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-09-03 18:21:25 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2- assignmnets/M7/Assignment-1 3/Solution.java
+++ CSPP2- assignmnets/M7/Assignment-1 3/Solution.java
@@ -1,57 +1,58 @@
 /**
  * Program to find whether the given input string  has minimum of 6 characters
  */
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for input validator.
  */
 class InputValidator {
-	/**.
-	 * { var_description }
-	 */
-	String str;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      input  The input
-	 */
-	public InputValidator(final String input) {
-		/**.
-		 * { item_description }
-		 */
-		this.str = input;
+    /**.
+     * { var_description }
+     */
+        String str;
+    /**.
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
+    public InputValidator(final String input) {
+        /**.
+         * { item_description }
+         */
+        this.str = input;
 
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      input  The input
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static boolean validateData(final String input) {
-		/**.
-		 * { item_description }
-		 */
-		return input.length() >= 6;
-	}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      input  The input
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static boolean validateData(final String input) {
+        /**.
+         * { item_description }
+         */
+        return input.length() >= 6;
+    }
 
 }
 /**.
  * Class for solution.
  */
 public final class Solution {
-	/**.
-	 * { item_description }
-	 */
-	public static void main(final String args[]) {
-		/**.
-		 * { var_description }
-		 */
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData(input));
+    /**.
+     * { item_description }
+     */
+
+    public static void main(final String args[]) {
+        /**.
+         * { var_description }
+         */
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println(i.validateData(input));
 
     }
 
