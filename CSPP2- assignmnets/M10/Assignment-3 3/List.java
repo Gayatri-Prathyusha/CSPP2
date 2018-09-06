906d1a51cb20182179108fdcb2cd4f1116d3758e
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-09-06 14:09:53 +0530

    "check_style_score -> 5 of 6 passed. style score: 0/1 "

--- CSPP2- assignmnets/M10/Assignment-3 3/List.java
+++ CSPP2- assignmnets/M10/Assignment-3 3/List.java
@@ -94,7 +94,7 @@ public class List<E> {
             for(int i = idex; i < size - 1; i++) {
                 list[i] = list[i + 1];
             }
-            //list[idex] = null;
+            list[size - 1] = null;
             size--;
         } else {
             System.out.println("Invalid Position Exception");
