6acc65d34e79e829fc2f401d36d9680476abd203
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-09-21 10:39:29 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2- assignmnets/Module 23/m23/Assignment-1 5/Solution.java
+++ CSPP2- assignmnets/Module 23/m23/Assignment-1 5/Solution.java
@@ -10,71 +10,75 @@ public class Solution {
     ArrayList<String> input = new ArrayList<>();
     ArrayList<String> inputnames = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-    String string = sc.next();
-    File file = new File(string);
-    File[] listOfFiles = file.listFiles();
-    for (File file1:listOfFiles) {
-      try {
-        inputnames.add(file1.getName());
-        Scanner sc1 = new Scanner(file1);
-        String s = "";
-        while (sc1.hasNext()) {
-          s += sc1.nextLine() + " ";
+    try {
+      String string = sc.next();
+      File file = new File(string);
+      File[] listOfFiles = file.listFiles();
+      for (File file1:listOfFiles) {
+        try {
+          inputnames.add(file1.getName());
+          Scanner sc1 = new Scanner(file1);
+          String s = "";
+          while (sc1.hasNext()) {
+            s += sc1.nextLine() + " ";
+          }
+          input.add(s);
+        } catch (FileNotFoundException e) {
+          System.out.println(e.getMessage());
         }
-        input.add(s);
-      } catch (FileNotFoundException e) {
-        System.out.println("empty directory");
       }
-    }
-    String[] inputarray = new String[inputnames.size()]; 
-    inputarray = inputnames.toArray(inputarray);
-    Arrays.sort(inputarray);
-    ArrayList<Integer> results = new ArrayList<>();
-    double resulttemp = 0.0;
-    int max = 0;
-    String s5 = "";
-    for (int i = 0; i < input.size(); i++) {
-      String s1 = input.get(i);
-      Bagofwords bag1 = new Bagofwords(s1);
-      bag1.cleanWords();
-      bag1.calculateFrequency();
-      for (int j = 0; j < input.size(); j++) {
-        String s2 = input.get(j);
-        Bagofwords bag2 = new Bagofwords(s2);
-        bag2.cleanWords();
-        bag2.calculateFrequency();
+      String[] inputarray = new String[inputnames.size()]; 
+      inputarray = inputnames.toArray(inputarray);
+      Arrays.sort(inputarray);
+      ArrayList<Integer> results = new ArrayList<>();
+      double resulttemp = 0.0;
+      int max = 0;
+      String s5 = "";
+      for (int i = 0; i < input.size(); i++) {
+        String s1 = input.get(i);
+        Bagofwords bag1 = new Bagofwords(s1);
+        bag1.cleanWords();
+        bag1.calculateFrequency();
+        for (int j = 0; j < input.size(); j++) {
+          String s2 = input.get(j);
+          Bagofwords bag2 = new Bagofwords(s2);
+          bag2.cleanWords();
+          bag2.calculateFrequency();
 
-        resulttemp = bag1.crossProduct(bag2.getMap()) * 100;
-        int a = (int) Math.round(resulttemp);
-        if (max < a && i != j) {
-          max = a;
-          s5 = "Maximum similarity is between " + inputarray[i] + " and " + inputarray[i];
-        }
-        results.add(a);
-      }
-    }
-    String s3 = "      \t";
-    //System.out.println(inputarray.toString());
-    for (String r:inputarray) {
-      s3 += r + "\t";
-    }
-    System.out.println(s3);
-    int k = 0;
-    for (int i = 0; i < inputarray.length; i++) {
-      String s4 = inputarray[i] + "\t";
-      if (i != 0) {
-        k = i * 4 + i;
-      }
-      for (int j = k; j < results.size(); j++) {
-        // System.out.println(j);
-        s4 += results.get(j) + "\t\t";
-        if ((j + 1)%5 == 0 && j != 0) {
-          break;
+          resulttemp = bag1.crossProduct(bag2.getMap()) * 100;
+          int a = (int) Math.round(resulttemp);
+          if (max < a && i != j) {
+            max = a;
+            s5 = "Maximum similarity is between " + inputarray[i] + " and " + inputarray[i];
+          }
+          results.add(a);
         }
       }
-      System.out.println(s4);
+      String s3 = "\t\t";
+      //System.out.println(inputarray.toString());
+      for (String r:inputarray) {
+        s3 += r + "\t";
+      }
+      System.out.println(s3);
+      int k = 0;
+      for (int i = 0; i < inputarray.length; i++) {
+        String s4 = inputarray[i] + "\t";
+        if (i != 0) {
+          k = i * 4 + i;
+        }
+        for (int j = k; j < results.size(); j++) {
+          // System.out.println(j);
+          s4 += results.get(j) + "\t\t";
+          if ((j + 1)%5 == 0 && j != 0) {
+            break;
+          }
+        }
+        System.out.println(s4);
+      }
+      System.out.println(s5);
+    } catch (Exception e) {
+      System.out.println("empty directory");
     }
-    System.out.println(s5);
   }
 }
 
